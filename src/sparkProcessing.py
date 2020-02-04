38a90d0676fbdbb4d14955160aa73f50480c1aa7
Mandy <dinhbl@uw.edu>
2020-02-03 23:15:32 -0800

    use variable for column names

--- src/sparkProcessing.py
+++ src/sparkProcessing.py
@@ -17,37 +17,78 @@ def write_to_postgres(df, table_name):
 
     df.write.jdbc(url=pg_url, table=table_name, mode="append", properties=pg_properties)
 
+
 if __name__ == "__main__":
 
     spark = SparkSession.builder.appName("BikeShare").getOrCreate()
-
     s3_prefix = 's3a://'
-    s3_bucket_capital_bike = 'capitalbikesharedata'
-    s3_file_name = '/*.csv'
+    s3_buckets_list = ['citibikenydata', 'capitalbikesharedata']
+    # s3_file_name = '/*.csv'
+    column_name = \
+        {\
+        'citibikenydata':\
+            {\
+            'company_name': 'CitiBike_New_York',\
+            'start_date': 'starttime',\
+            'member_type': 'usertype',\
+            'duration' : 'tripduration',\
+            'start_station' : 'start station id',\
+            'end_station': 'end station id',\
+            'bike_number' : 'bikeid'\
+            },\
+        'capitalbikesharedata':\
+            {\
+            'company_name': 'Capital_BikeShare_Metro_DC',\
+            'start_date': 'Start date',\
+            'member_type': 'Member type',\
+            'duration' : 'Duration',\
+            'start_station' : 'Start station number',\
+            'end_station': 'End station number',\
+            'bike_number' : 'Bike number'\
+            }\
+        }
 
-    s3_url_capital_bike = s3_prefix + s3_bucket_capital_bike + s3_file_name
+    for bucket in s3_buckets_list:
+        if bucket == 'citibikenydata':
+            s3_file_name = '/201605-citibike-tripdata.csv'
+        else:
+            s3_file_name = '/2010-capitalbikeshare-tripdata.csv'
 
-    df_capital_bike = spark.read.csv(s3_url_capital_bike, header=True, inferSchema=True)
+        s3_bucket_name = bucket
+        s3_url = s3_prefix + s3_bucket_name + s3_file_name
+        df = spark.read.csv(s3_url, header=True, inferSchema=True)
+        df.createOrReplaceTempView("sql_view")
+        processed_data = spark.sql(\
+            "SELECT '{0}' as company_name,\
+                date_trunc('hour', `{1}`) as ride_date,\
+                (dayofweek(date_trunc('day', `{1}`))) as ride_dow,\
+                case when `{2}` = 'Subscriber' then 'Member' when `{2}` = 'Customer' then 'Casual'\
+                else `{2}` end as member,\
+                count(*) as total_rides,\
+                (sum(case when `{3}` <= (30*60) then 1 else 0 end)) as total_rides_under_30min,\
+                (sum(case when `{3}` > (30*60) then 1 else 0 end)) as total_rides_greater_30min,\
+                int(round(sum(`{3}`/3600),0)) as total_duration_hours,\
+                (count(distinct(`{4}`)) + count(distinct(`{5}`))) as total_stations,\
+                count(distinct(`{6}`)) as total_bikes\
+            FROM sql_view\
+            GROUP BY\
+                date_trunc('hour', `{1}`),\
+                (dayofweek(date_trunc('day', `{1}`))),\
+                `{2}`\
+            ORDER BY\
+                (date_trunc('hour', `{1}`)),\
+                (dayofweek(date_trunc('day', `{1}`))),\
+                `{2}`"\
+                .format(\
+                column_name[s3_bucket_name]['company_name'],\
+                column_name[s3_bucket_name]['start_date'],\
+                column_name[s3_bucket_name]['member_type'],\
+                column_name[s3_bucket_name]['duration'],\
+                column_name[s3_bucket_name]['start_station'],\
+                column_name[s3_bucket_name]['end_station'],\
+                column_name[s3_bucket_name]['bike_number']\
+            ))
 
-    df_capital_bike.createOrReplaceTempView("sql_view_capital")
-
-    df_output = spark.sql("SELECT 'CapitalBike' as company_name,\
-            date_trunc('day', `Start Date`) as ride_date,\
-            (dayofweek(date_trunc('day', `Start Date`))) as ride_DOW,\
-            `Member type` as member,\
-            count(*) as total_rides,\
-            (sum(case when `Duration` <= (30*60) then 1 else 0 end)) as total_rides_under_30min,\
-            (sum(case when `Duration` > (30*60) then 1 else 0 end)) as total_rides_greater_30min,\
-            int(round(sum((`Duration`)/3600),0)) as total_duration_minutes,\
-            (count(distinct(`Start station number`)) + count(distinct(`End station number`))) as total_stations,\
-            count(distinct(`Bike number`)) as total_bikes\
-        	FROM sql_view_capital\
-        	GROUP BY date_trunc('day', `Start Date`),\
-            (dayofweek(date_trunc('day', `Start Date`))),\
-            `Member type`\
-        	ORDER BY (date_trunc('day', `Start Date`)),\
-            `Member type`")
-
-    write_to_postgres(df_output, "capital")
+    write_to_postgres(processed_data, "postgres_data")
 
     spark.stop()
